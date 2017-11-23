edb7ba374e7fba1100b11af9b6124122dc87ad8e
Ural Davletshin <ural.davletshin@gmail.com>
2017-11-24 01:46:50 +0300

    Job list container

--- app/containers/JobListConnected/saga.js
+++ app/containers/JobListConnected/saga.js
@@ -1,7 +1,7 @@
-import { call, put, select, takeLatest } from 'redux-saga/effects';
+import { put, takeLatest } from 'redux-saga/effects';
 
-import { JOB_LIST_REQUEST, JOB_LIST_SUCCESS } from './constants';
-import { jobListRequest, jobListSuccess } from './actions';
+import { JOB_LIST_REQUEST } from './constants';
+import { jobListSuccess } from './actions';
 
 export function* loadJobs() {
   yield put(jobListSuccess(['new job1', 'new job2']));
@@ -15,7 +15,5 @@ export default function* jobListData() {
   // By using `takeLatest` only the result of the latest API call is applied.
   // It returns task descriptor (just like fork) so we can continue execution
   // It will be cancelled automatically on component unmount
-  console.log('root saga');
-
   yield takeLatest(JOB_LIST_REQUEST, loadJobs);
 }
